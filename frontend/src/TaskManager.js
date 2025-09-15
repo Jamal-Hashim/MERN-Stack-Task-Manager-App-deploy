c0f07d46a98abd220b1fb2b1dc72a48229399f96
Jamal-Hashim <jamalhahsim9120@gmail.com.com>
2025-09-15 14:50:17 +0530

    Updated feature / bug fix

--- frontend/src/TaskManager.js
+++ frontend/src/TaskManager.js
@@ -79,6 +79,7 @@ function TaskManager() {
 
             if (success) {
                 // show succes toast
+                setTasks((prevTasks) => prevTasks.filter((task) => task._id !== id));
                 notify(message, 'success');
             } else {
                 // show error toast
@@ -146,56 +147,100 @@ function TaskManager() {
     }
 
     return (
-        <div className='d-flex flex-column align-items-center w-50 m-auto mt-5'>
-            <h1 className='mb-4'>Task Manager</h1>
 
-            {/* Input and Search Box */}
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-10 col-lg-8">
 
-            <div className='d-flex justify-content-between align-items-center mb-4 w-100'>
-                <div className='input-group flex-grow-1 me-2'>
-                    <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder='Add a new Task' className='form-control mw-1' />
-                    <button onClick={handleTask} className='btn btn-success btn-sm me-2'>
-                        <FaPlus className='m-2' />
-                    </button>
-                </div>
-                <div className='input-group flex-grow-1'>
-                    <span className='input-group-text'>
-                        <FaSearch />
-                    </span>
-                    <input onChange={handleSearch} type="text" placeholder='Search Task' className='form-control mw-1' />
-                </div>
-            </div>
+                    <h1 className="mb-4 text-center">Task Manager</h1>
 
-            {/* Lists of items */}
-
-            <div className='d-flex flex-column w-100'>
-                {
-                    tasks.map((item) => (
-                        <div key={item._id} className="m-2 p-2 border bg-light w-100 rounded-3 d-flex justify-content-between align-items-center flex-wrap">
-                            <span className={item.isDone ? 'text-decoration-line-through' : ''}>{item.taskName}</span>
-                            <div className="mt-2 mt-sm-0">
-                                <button onClick={() => handleCheckAndUncheck(item)}
-                                    className='btn btn-success btn-sm me-2' type='button'>
-                                    <FaCheck />
-                                </button>
-                                <button onClick={() => setUpdateTask(item)}
-                                    className='btn btn-primary btn-sm me-2' type='button'>
-                                    <FaPenAlt />
-                                </button>
-                                <button onClick={() => handleDeleteTask(item._id)}
-                                    className='btn btn-danger btn-sm me-2' type='button'>
-                                    <FaTrash />
+                    {/* Input and Search Box */}
+                    <div className="row g-2 mb-4">
+                        <div className="col-12 col-md-6">
+                            <div className="input-group">
+                                <input
+                                    value={input}
+                                    onChange={(e) => setInput(e.target.value)}
+                                    type="text"
+                                    placeholder="Add a new Task"
+                                    className="form-control"
+                                />
+                                <button
+                                    onClick={handleTask}
+                                    className="btn btn-success btn-sm"
+                                    type="button"
+                                >
+                                    <FaPlus className="m-1" />
                                 </button>
                             </div>
                         </div>
-                    ))
-                }
+
+                        <div className="col-12 col-md-6">
+                            <div className="input-group">
+                                <span className="input-group-text">
+                                    <FaSearch />
+                                </span>
+                                <input
+                                    onChange={handleSearch}
+                                    type="text"
+                                    placeholder="Search Task"
+                                    className="form-control"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Lists of items */}
+                    <div className="d-flex flex-column w-100">
+                        {tasks.map((item) => (
+                            <div
+                                key={item._id}
+                                className="m-2 p-3 border bg-light rounded-3 d-flex justify-content-between align-items-center flex-wrap"
+                            >
+                                <span
+                                    className={
+                                        item.isDone ? "text-decoration-line-through" : ""
+                                    }
+                                >
+                                    {item.taskName}
+                                </span>
+                                <div className="mt-2 mt-sm-0 d-flex flex-wrap">
+                                    <button
+                                        onClick={() => handleCheckAndUncheck(item)}
+                                        className="btn btn-success btn-sm me-2 mb-2 mb-sm-0"
+                                        type="button"
+                                    >
+                                        <FaCheck />
+                                    </button>
+                                    <button
+                                        onClick={() => setUpdateTask(item)}
+                                        className="btn btn-primary btn-sm me-2 mb-2 mb-sm-0"
+                                        type="button"
+                                    >
+                                        <FaPenAlt />
+                                    </button>
+                                    <button
+                                        onClick={() => handleDeleteTask(item._id)}
+                                        className="btn btn-danger btn-sm mb-2 mb-sm-0"
+                                        type="button"
+                                    >
+                                        <FaTrash />
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* Toastify */}
+                    <ToastContainer
+                        position="top-right"
+                        autoClose={3000}
+                        hideProgressBar={false}
+                    />
+                </div>
             </div>
-
-            {/* Toastify */}
-
-            <ToastContainer position='top-right' autoClose={3000} hideProgressBar={false} />
         </div>
+
     )
 }
 export default TaskManager;
